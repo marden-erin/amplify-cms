c545b3cc2a7c770f1f3e10acb8206b5f984cdd88
marden-erin <emarden@nevada.unr.edu>
2024-08-18 19:41:10 -0600

    changes for amplify

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+      <div className='App'>
+      <header className='App-header'>
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <h1>hello cms</h1>
+          </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 
